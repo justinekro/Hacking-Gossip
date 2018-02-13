f63c442d434a97eec7f52d009c72d55f1a1d4904
Justine <>
2018-02-13 12:49:56 +0100

    added code to sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_many :gossips, dependent: :destroy
+  
+  attr_accessor :sign_up_code
+  validates :sign_up_code,
+    on: :create,
+    presence: true,
+    inclusion: { in: ["GOSSIP2017"] }
+
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
